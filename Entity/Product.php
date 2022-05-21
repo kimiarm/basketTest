67e4738d2f64e5292d2a362fe0ef25f76bba110a
kimiarm <kimia.rezaei96@gmail.com>
2022-05-21 18:30:00 +0430

    fix immmutable product class

--- Entity/Product.php
+++ Entity/Product.php
@@ -5,11 +5,13 @@ class Product {
     private $price;
     private $title;
     private $discount;
-
+// we should write validation classes  for each attribute
     public function __construct(string $title, int $price, int $discount = 0) {
         if($title == "") {
             return;
             // throw custom exception throw couldNotCreateProduct.withAttributes(['title' => $title, 'price' => $price, 'discount' => $discount]) 
+
+             
         }
         if($price <  0) {
             return;
@@ -44,6 +46,45 @@ class Product {
     public function title() : int
     {
         return $this->title;
+    }
+
+    public function withTitle(string $title) : Product
+    {
+        if($title == "") {
+
+            // throw custom exception throw couldNotCreateProduct.withAttributes(['title' => $title, 'price' => $price, 'discount' => $discount]) 
+        }
+
+        $newProduct = clone $this;
+        $newProduct->title = $title;
+
+        return $newProduct;
+    }
+
+    public function withDiscount(int $discount) : Product
+    {
+        if($discount <  0 || $discount > 100) {
+
+            // throw custom exception throw couldNotCreateProduct.withAttributes(['title' => $title, 'price' => $price, 'discount' => $discount]) 
+        }
+
+        $newProduct = clone $this;
+        $newProduct->discount = $discount;
+
+        return $newProduct;
+    }
+
+    public function withPrice(int $price) : Product
+    {
+        if($price <  0) {
+
+            // throw custom exception throw couldNotCreateProduct.withAttributes(['title' => $title, 'price' => $price, 'discount' => $discount]) 
+        }
+
+        $newProduct = clone $this;
+        $newProduct->price = $price;
+
+        return $newProduct;
     }
 
 }
